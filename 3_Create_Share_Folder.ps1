0a1f6c1ced0632a11322b22ed2d4de16bb76cb21
Rawin <100992113+VolkRawinUR@users.noreply.github.com>
2025-05-15 16:55:29 +0700

    Add files via upload

---
+++ 3_Create_Share_Folder.ps1
@@ -0,0 +1,22 @@
+﻿$Username_Prefix = 'INF'
+$Number_of_User = 20
+$Start_Number_of_User = 1
+$Number_of_Digit = 2
+$RootPath='D:\'
+
+For ($i = $Start_Number_of_User; $i -le $Number_of_User; $i++) {
+	$Username=$Username_Prefix + "{0:d$Number_of_digit}" -f $i
+	$FolderName=$Username
+	$TargetPath=$RootPath + $FolderName
+	If (!(Test-Path $TargetPath))
+	{
+		New-Item -Path $RootPath -Name $FolderName -ItemType "directory"
+	}
+
+	$ShareParameters = @{
+		Name = $FolderName
+		Path = $TargetPath
+		FullAccess = $Username
+	}
+	New-SmbShare @ShareParameters
+}
---
+++ map_drive.bat
@@ -0,0 +1,1 @@
+powershell.exe -command "& 'D:\map_drive.ps1'"
